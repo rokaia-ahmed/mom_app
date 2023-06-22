ebbb3e236cbdf9457cb076ae4824226640ca8bd1
rokia <rokiaahmed42@gmail.com>
2023-06-22 21:30:18 +0300

    make splash screen

---
+++ lib/view/layout/layout_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LayoutScreen extends StatelessWidget {
+  const LayoutScreen({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/view/splash/splash_screen.dart
+++ lib/view/splash/splash_screen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mom_app/core/utils/media_query_values.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+          child: Image.asset('assets/images/splash.png',
+          width:context.width*0.6 ,
+           height:context.height*0.5 ,
+            fit: BoxFit.cover,
+          ),
+      ),
+    );
   }
 }
