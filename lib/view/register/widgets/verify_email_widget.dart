41de5922301714c4bab77e6cb6e5f95bad6d55e0
rokaia ahmed <rokiaahmed42@gmail.com>
2023-08-13 02:12:59 +0300

    handle weight

--- lib/view/register/widgets/verify_email_widget.dart
+++ lib/view/register/widgets/verify_email_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mom_app/core/utils/media_query_values.dart';
 import '../../../core/utils/app_colors.dart';
 
@@ -18,67 +19,40 @@ class VerifyEmailWidget extends StatelessWidget {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Container(
-          width: context.width*0.15,
-          height: context.height *0.07,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color:AppColors.lightGreen,
-          ),
-          child:TextFormField(
-            controller: num1,
-            decoration:const InputDecoration(
-              border:InputBorder.none,
-              contentPadding: EdgeInsets.only(left: 10),
-            ),
-          ) ,
-        ),
-        Container(
-          width: context.width*0.15,
-          height: context.height *0.07,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color:AppColors.lightGreen,
-          ),
-          child:TextFormField(
-            controller: num2,
-            decoration:const InputDecoration(
-              border:InputBorder.none,
-              contentPadding: EdgeInsets.only(left: 10),
-            ),
-          ) ,
-        ),
-        Container(
-          width: context.width*0.15,
-          height: context.height *0.07,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color:AppColors.lightGreen,
-          ),
-          child:TextFormField(
-            controller: num3,
-            decoration:const InputDecoration(
-              border:InputBorder.none,
-              contentPadding: EdgeInsets.only(left: 10),
-            ),
-          ) ,
-        ),
-        Container(
-          width: context.width*0.15,
-          height: context.height *0.07,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color:AppColors.lightGreen,
-          ),
-          child:TextFormField(
-            controller: num4,
-            decoration:const InputDecoration(
-              border:InputBorder.none,
-              contentPadding: EdgeInsets.only(left: 10),
-            ),
-          ) ,
-        ),
+        BuildContainer(controller:num1 ),
+        BuildContainer(controller:num2 ),
+        BuildContainer(controller:num3 ),
+        BuildContainer(controller:num4 ),
       ],
     );
   }
 }
+
+class BuildContainer extends StatelessWidget {
+  const BuildContainer({super.key, required this.controller});
+   final TextEditingController controller ;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: context.width*0.15,
+      height: context.height *0.07,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color:AppColors.lightGreen,
+      ),
+      child:TextFormField(
+        controller: controller,
+        maxLength:1 ,
+        textAlign: TextAlign.center,
+        keyboardType:TextInputType.number,
+        inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))],
+        decoration:const InputDecoration(
+          counter:Offstage() ,
+          border:InputBorder.none,
+          contentPadding: EdgeInsets.only(left: 10),
+        ),
+      ) ,
+    );
+  }
+}
+
